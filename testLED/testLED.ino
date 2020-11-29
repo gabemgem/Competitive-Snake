dd3129a7bc5466dff3059584735d699b93a74525
Gabe Maayan <gabemgem@gmail.com>
2020-11-29 14:41:00 -0500

    Change to use 56 leds

--- testLED/testLED.ino
+++ testLED/testLED.ino
@@ -1,11 +1,11 @@
 #include <FastLED.h>
-CRGB leds[64];
+CRGB leds[56];
 
 void setup() {
   // put your setup code here, to run once:
   FastLED.setBrightness(100);
-  FastLED.addLeds<WS2812, 13, GRB>(leds, 64);
-  fill_solid(leds, 64, CRGB::Grey);
+  FastLED.addLeds<WS2812, 13, GRB>(leds, 56);
+  fill_solid(leds, 56, CRGB::Grey);
   FastLED.show();
 }
 
